5c5c3d1f01e99f31cbf98ef59602cae46e657b28
Cerbur <1051140642@qq.com>
2020-05-19 03:15:18 +0800

    优化页面控制层的逻辑

--- src/main.c
+++ src/main.c
@@ -1,57 +1,73 @@
 #include "./include/client.h"
+#include "./include/utils.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define clear "clear"
 /**
  * 登录态规范 0->未登录 1已登录
  */
-char *session_login_schoolId = "3118007450";   //一个全局指针,做用户登录态保持
-int stack = 1;  //表示当前页面所在的操作层
 int main(int argc, char const *argv[])
 {
+    client_file_init();
+    int stack = 0;  //表示当前页面所在的操作层
+    char session_login_schoolId[100];// = "3118007450";   //一个全局指针,做用户登录态保持
     // TODO 这里写基础逻辑
     while (1)
     {
-        switch (stack)
-        {
+        while (stack == 0)
+        {   
+            system(clear);
             int choice;
             int status;
-            case 0:
-                client_welcome(&choice);
-                switch (choice) {
-                case 1:
-                    status = -2;
-                    while (status == -2) {
-                        status = client_user_login(&session_login_schoolId);
-                    }
-                    if (status == 0) {
-                        break;
-                    }
-                    if (status == 1) {
-                        stack = 1;
-                        printf("学号:%s登录成功",session_login_schoolId);
-                        break;
-                    }
-                case 2:
-                    status = client_registered(&session_login_schoolId);
-                    if (status) {
-                        stack = 1;
-                    }
-                    break;
-                case 3:
-                    client_about_author();
-                    break;
-                case 4:
-                    client_exit();
+            client_welcome(&choice);
+            switch (choice) {
+            case 1:
+                status = -2;
+                while (status == -2) {
+                    system(clear);
+                    status = client_user_login(session_login_schoolId);
+                }
+                if (status == 0) {
+                    system(clear);
                     break;
                 }
+                if (status == 1) {
+                    stack = 1;
+                    printf("学号:%s登录成功,正在跳转",session_login_schoolId);
+                    printf("...");
+                    system(clear);
+                    break;
+                }
+            case 2:
+                system(clear);
+                status = client_registered(session_login_schoolId);
+                if (status) {
+                    stack = 1;
+                }
                 break;
-            case 1:
-                stack = 0;
-                client_user_page(session_login_schoolId,&choice);
-                // printf("登录成功");
+            case 3:
+                client_about_author();
                 break;
+            case 4:
+                client_exit();
+                break;
+            }
         }
         
-
+        while (stack == 1)
+        {   
+            system(clear);
+            int choice;
+            client_user_page(session_login_schoolId,&choice);
+            switch (choice)
+            {
+                case 3:
+                    system(clear);
+                    client_post_lost_property(session_login_schoolId);
+                    break;
+            }
+        }
     }
     
     return 0;
